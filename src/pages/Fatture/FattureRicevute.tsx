ea0d18cd6cdd867b7c811c1fa7d46280894d3a3c
root <root@vmi817237.contaboserver.net>
2025-11-14 12:32:13 +0100

    Reset workspace clean (applica .gitignore)

--- src/pages/Fatture/FattureRicevute.tsx
+++
@@ -1,5 +0,0 @@
-// src/pages/Fatture/FattureRicevute.tsx  (RB Consulenza)
-import FattureList from "./FattureList";
-export default function FattureRicevute() {
-  return <FattureList which="ricevute" />;
-}
--- src/pages/Fatture/FattureRicevuteNew.tsx
+++
@@ -1,5 +0,0 @@
-// src/pages/Fatture/FattureRicevuteNew.tsx  (RB Intermediari)
-import FattureList from "./FattureList";
-export default function FattureRicevuteNew() {
-  return <FattureList which="ricevutenew" />;
-}
